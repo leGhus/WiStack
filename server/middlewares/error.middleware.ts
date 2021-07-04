4537828051f0f4d8fa796668a285ccd5c89ab6a6
ghouis <guillaume.houis@gmail.com>
2021-07-04 22:19:55 +0200

    format

--- server/middlewares/error.middleware.ts
+++ server/middlewares/error.middleware.ts
@@ -1,15 +1,21 @@
 import express from 'express';
 import { CustomError, WS_ERRORS } from '../others/errors';
 
-export function errorMiddleware(error: CustomError, req: express.Request, res: express.Response): void {
+export function errorMiddleware(
+  error: CustomError,
+  req: express.Request,
+  res: express.Response,
+): void {
   if (req && error) {
     if (
-      error?.name === WS_ERRORS.BAD_REQUEST
-      || error?.name === WS_ERRORS.UNAUTHORIZED
-      || error?.name === WS_ERRORS.FORBIDDEN
-      || error?.name === WS_ERRORS.ERROR
+      error?.name === WS_ERRORS.BAD_REQUEST ||
+      error?.name === WS_ERRORS.UNAUTHORIZED ||
+      error?.name === WS_ERRORS.FORBIDDEN ||
+      error?.name === WS_ERRORS.ERROR
     ) {
-      res.status(error.statusCode ?? 500).send({ error: { name: error.name, message: error.message } });
+      res
+        .status(error.statusCode ?? 500)
+        .send({ error: { name: error.name, message: error.message } });
       return;
     }
     res.status(500).send({ error: WS_ERRORS.ERROR });
